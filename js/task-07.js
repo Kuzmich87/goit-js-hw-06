c885b86145c44ac6b5037b3501a0e9cf055e4a31
Kuzmich87 <d.kuzmich87@gmail.com>
2022-08-23 10:16:11 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -3,10 +3,9 @@ const rev = {
     input: document.getElementById("font-size-control"),
     spanText: document.getElementById("text"),
 }
-rev.spanText.style.fontSize = "56px";
-rev.input.addEventListener('change', onInputHandRange);
+rev.spanText.style.fontSize = rev.input.value+ "px" ;
+rev.input.addEventListener('input', onInputHandRange);
 
 function onInputHandRange(event) {
   rev.spanText.style.fontSize = event.currentTarget.value + "px";
 }
-
